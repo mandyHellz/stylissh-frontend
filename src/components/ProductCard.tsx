c7b678d1cfca30a67a84b75e4aac13671fd93c78
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-07 19:45:44 -0300

    feat: add link to product page with slug id

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,20 +1,24 @@
-/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+
 interface ProductProps {
   title: string;
   price: number;
   image: string;
+  slug: string;
 }
 
-export const ProductCard = ({ title, image, price }: ProductProps) => {
+export const ProductCard = ({ title, image, price, slug }: ProductProps) => {
   return (
     <div className="relative flex flex-col p-6 bg-white">
-      <div>
-        <img
-          src={image}
-          alt=""
-          className="mx-auto w-full cursor-pointer hover:opacity-80 transition duration-300 ease-in"
-        />
-      </div>
+      <Link href={`/product/${slug}`}>
+        <div>
+          <img
+            src={image}
+            alt=""
+            className="mx-auto w-full cursor-pointer hover:opacity-80 transition duration-300 ease-in"
+          />
+        </div>
+      </Link>
       <h2 className="py-1 font-medium">{title}</h2>
       <h3>{price}</h3>
     </div>
