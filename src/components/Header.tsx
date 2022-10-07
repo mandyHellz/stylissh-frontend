acfdc255c1baa2163a46a69c05d45448ed076c06
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-07 19:51:46 -0300

    style: adjusted styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,17 @@ import { FiShoppingBag } from "react-icons/fi";
 
 export const Header = () => {
   return (
-    <div className="w-full flex justify-between items-center h-16 px-4 bg-white">
-      <Link href={"/"}>
-        <p className="text-3xl cursor-pointer">Styled.</p>
-      </Link>
-      <Link href={"/"}>
-        <div className="relative cursor-pointer">
-          <FiShoppingBag className="" size={25} />
-        </div>
-      </Link>
+    <div className="w-full bg-white h-16">
+      <div className="max-w-container mx-auto flex justify-between items-center h-16 px-10">
+        <Link href={"/"}>
+          <p className="text-3xl cursor-pointer">Styled.</p>
+        </Link>
+        <Link href={"/"}>
+          <div className="relative cursor-pointer px-1">
+            <FiShoppingBag className="" size={25} />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
