a5d1b291cd956c751015de857651a25dbbc4ae09
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-08 17:12:12 -0300

    feat: add shop provider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { Provider, createClient } from "urql";
+import { ShopProvider } from "../contexts/ShopContext";
 import "../styles/globals.css";
 
 const client = createClient({ url: `${process.env.NEXT_PUBLIC_BACKEND_API}` });
@@ -7,7 +8,9 @@ const client = createClient({ url: `${process.env.NEXT_PUBLIC_BACKEND_API}` });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider value={client}>
-      <Component {...pageProps} />
+      <ShopProvider>
+        <Component {...pageProps} />
+      </ShopProvider>
     </Provider>
   );
 }
