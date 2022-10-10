6ea7f82053ffd073cf8eff3416451ab027249460
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-10 14:04:17 -0300

    feat: add show cart functions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,25 @@
 import Link from "next/link";
 import { FiShoppingBag } from "react-icons/fi";
+import { useShopContext } from "../contexts/ShopContext";
+import { Cart } from "./Cart";
 
 export const Header = () => {
+  const { showCart, showCartHandler } = useShopContext();
+
   return (
-    <div className="w-full bg-white h-16">
-      <div className="max-w-container mx-auto flex justify-between items-center h-16 px-10">
+    <div className="w-full h-16">
+      <div className="max-w-container mx-auto flex justify-between items-center h-16 px-10 z-10">
         <Link href={"/"}>
           <p className="text-3xl cursor-pointer">Styled.</p>
         </Link>
-        <Link href={"/"}>
-          <div className="relative cursor-pointer px-1">
-            <FiShoppingBag className="" size={25} />
-          </div>
-        </Link>
+        <div
+          className="relative cursor-pointer px-1"
+          onClick={() => showCartHandler(true)}
+        >
+          <FiShoppingBag className="" size={25} />
+        </div>
       </div>
+      {showCart && <Cart />}
     </div>
   );
 };
