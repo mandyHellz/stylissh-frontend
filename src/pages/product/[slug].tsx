359962f637c434f4f6dd47a91a6eff393a5a61eb
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-10 14:03:13 -0300

    refactor: add quantity component

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -2,17 +2,16 @@ import { useRouter } from "next/router";
 import { useQuery } from "urql";
 import { GET_PRODUCT_DETAILS_QUERY } from "../../lib/query";
 import { Product } from "../../types/product";
-import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
 import { DefaultLayout } from "../../components/DefaultLayout";
 import { useShopContext } from "../../contexts/ShopContext";
+import { QuantitySelector } from "../../components/QuantitySelector";
 
 const ProductDetails = () => {
   const {
     productQuantity,
     increaseProductQuantity,
     decreaseProductQuantity,
-    cartItems,
-    onAddToCart,
+    addItemToCart,
   } = useShopContext();
   const router = useRouter();
   const [results] = useQuery({
@@ -43,29 +42,23 @@ const ProductDetails = () => {
               className="w-full sm:w-2/5"
             />
             <div className="flex flex-col gap-10 w-full sm:w-2/5">
-              <h2 className="font-medium capitalize">{product.title}</h2>
+              <h2 className="font-medium capitalize text-primary">
+                {product.title}
+              </h2>
               <p>{product.description}</p>
 
-              <div className="flex items-center">
-                <span className="text-secondary">Quantity</span>
-                <button
-                  onClick={decreaseProductQuantity}
-                  className="text-primary hover:text-secondary duration-300 bg-transparent px-4 py-2"
-                >
-                  <AiFillMinusCircle />
-                </button>
-                <p className="text-center">{productQuantity}</p>
-                <button
-                  onClick={increaseProductQuantity}
-                  className="text-primary hover:text-secondary duration-300 bg-transparent px-4 py-2"
-                >
-                  <AiFillPlusCircle />
-                </button>
-              </div>
+              <QuantitySelector
+                plusButtonHandler={() => increaseProductQuantity()}
+                minusButtonHandler={() => decreaseProductQuantity()}
+                quantity={productQuantity}
+              />
 
               <button
                 onClick={() =>
-                  onAddToCart({ product: response, quantity: productQuantity })
+                  addItemToCart({
+                    product: response,
+                    quantity: productQuantity,
+                  })
                 }
                 className="w-full bg-primary hover:bg-secondary duration-300 text-white font-medium text-xs sm:text-sm px-4 py-2 border"
               >
