424958a803fe0f2712d3d0e745d253544c601aaa
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-13 14:07:48 -0300

    style: update success page styling

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import { DefaultLayout } from "../components/DefaultLayout";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 interface SuccessProps {
   address: {
@@ -32,36 +34,58 @@ export default function Success({
 
   return (
     <DefaultLayout title={"Stylissh. | Thank u!"}>
-      <div>
-        <h1>Thank you for your order!</h1>
-        {email && (
-          <>
-            <p>A confirmation email has been sent to</p>
-            <p>{email}</p>
-          </>
-        )}
+      <motion.div
+        animate={{ opacity: 1, scale: 1 }}
+        initial={{ opacity: 0, scale: 0.75 }}
+        transition={{ duration: 0.75 }}
+        className="w-full mx-auto"
+      >
+        <div className="flex flex-col items-center bg-white rounded p-12 gap-4">
+          <div className="flex flex-col gap-4 font-medium text-center">
+            <h1 className="text-2xl">Thank you for your order!</h1>
+            {email && (
+              <div className="text-base">
+                <p>A confirmation email has been sent to</p>
+                <p className="text-sm">{email}</p>
+              </div>
+            )}
+          </div>
 
-        <div>
-          <p>Address:</p>
-          <p>{address.line1}</p>
-          <p>{address.city}</p>
-          <p>
-            {address.state} | {address.country}
-          </p>
-        </div>
-
-        <div>
-          <p>Products:</p>
-          {products.map((product) => (
-            <div key={product.id} className="flex flex-col max-w-xs">
-              <img src={product.images} alt={product.name} className="w-40" />
-              <p>{product.name}</p>
+          <div className="w-full flex flex-col sm:flex-row justify-center gap-8">
+            <div className="text-center">
+              <p className="font-medium">Address:</p>
+              <p className="text-sm">{address.line1}</p>
+              <p className="text-sm">{address.city}</p>
+              <p className="text-sm">
+                {address.state} | {address.country}
+              </p>
             </div>
-          ))}
-        </div>
 
-        <button onClick={() => router.push("/")}>Continue comprando</button>
-      </div>
+            <div className="text-center">
+              <p className="font-medium">Products:</p>
+              {products.map((product) => (
+                <p key={product.id} className="w-full truncate text-sm">
+                  {product.name}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <Image
+            width={300}
+            height={230}
+            alt="dog_success_page"
+            src="https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+
+          <button
+            className="bg-primary hover:bg-secondary transition-colors text-white text-sm mt-10 py-2 px-4"
+            onClick={() => router.push("/")}
+          >
+            Continue Shopping
+          </button>
+        </div>
+      </motion.div>
     </DefaultLayout>
   );
 }
