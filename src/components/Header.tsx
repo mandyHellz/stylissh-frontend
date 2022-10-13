30d17c05b9a62c698d9d2317c206138960a0e333
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-13 16:24:38 -0300

    feat: add user component to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Cart } from "./Cart";
+import { User } from "./User";
 import { useShopContext } from "../contexts/ShopContext";
+
 import { FiShoppingBag } from "react-icons/fi";
 import { AnimatePresence, motion } from "framer-motion";
 
@@ -13,20 +15,23 @@ export const Header = () => {
         <Link href={"/"}>
           <p className="text-3xl cursor-pointer">Stylissh.</p>
         </Link>
-        <div
-          className="relative cursor-pointer px-1"
-          onClick={() => showCartHandler(true)}
-        >
-          {totalCartItems > 0 && (
-            <motion.div
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              className="bg-red-500 w-5 h-5 flex items-center justify-center rounded-full absolute -top-1.5 -right-1.5"
-            >
-              <span className="text-white text-xxs">{totalCartItems}</span>
-            </motion.div>
-          )}
-          <FiShoppingBag className="" size={25} />
+        <div className="flex gap-4">
+          <div
+            className="relative cursor-pointer px-1"
+            onClick={() => showCartHandler(true)}
+          >
+            {totalCartItems > 0 && (
+              <motion.div
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                className="bg-red-500 w-5 h-5 flex items-center justify-center rounded-full absolute -top-1.5 -right-1.5"
+              >
+                <span className="text-white text-xxs">{totalCartItems}</span>
+              </motion.div>
+            )}
+            <FiShoppingBag size={25} />
+          </div>
+          <User />
         </div>
       </div>
       <AnimatePresence>{showCart && <Cart />}</AnimatePresence>
