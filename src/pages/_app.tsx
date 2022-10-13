6b4cf4954d0f76a6d899a5f130f652881a5b89d5
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-13 16:24:53 -0300

    feat: add user Provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 import type { AppProps } from "next/app";
 import { Provider, createClient } from "urql";
 import { ShopProvider } from "../contexts/ShopContext";
+import { UserProvider } from "@auth0/nextjs-auth0";
 import "../styles/globals.css";
 
 const client = createClient({ url: `${process.env.NEXT_PUBLIC_BACKEND_API}` });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider value={client}>
+    <UserProvider>
       <ShopProvider>
-        <Component {...pageProps} />
+        <Provider value={client}>
+          <Component {...pageProps} />
+        </Provider>
       </ShopProvider>
-    </Provider>
+    </UserProvider>
   );
 }
 
